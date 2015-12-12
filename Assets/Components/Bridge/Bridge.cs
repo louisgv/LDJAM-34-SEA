278473af24c16724334f099db389494a2eb070a6
Louis Graham Vichy <lgvichy@gmail.com>
2015-12-11 23:42:20 -0800

    Init Flower and Bridge

---
+++ Assets/Components/Bridge/Bridge.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bridge : MonoBehaviour
+{
+
+	public static float buildProgress = 0;
+	
+	public static float buildMAX = 3;
+	
+	public enum BridgeState
+	{
+		FINISHED,
+		UNFINISHED
+	}
+	
+	public BridgeState state = BridgeState.UNFINISHED;
+	
+	// There's only one bridge anyhow?
+	public static void BuildBridge ()
+	{
+		++buildProgress;
+	}
+	
+}
---
+++ Assets/Components/Flower/Flower.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Flower : MonoBehaviour
+{
+	[Range(99,450)]
+	public float
+		GROW_MAX = 99;
+
+	public float growthProgress = 0;
+	
+	public float growthForBridge = 45;
+	
+	[Range(1, 10)]
+	public float
+		growthSpeed = 1;
+	
+	public enum FlowerState
+	{
+		IS_GROWING,
+		CHOPPED_DEAD,
+		FULL_GROWN
+	}
+	
+	public FlowerState state = FlowerState.IS_GROWING;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	void BeFullGrown ()
+	{
+		state = FlowerState.FULL_GROWN;
+		// TODO: Game OVER, P1 Win. 
+		// ANIMATION with Flower full bloom
+	}
+	
+	void BeChoppedDead ()
+	{
+		state = FlowerState.CHOPPED_DEAD;
+		// TODO: Call the building bridge method, could be a static method.
+		if (growthProgress == growthForBridge) {
+			Bridge.ImproveBridge ();
+		}
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (state.Equals (FlowerState.IS_GROWING)) {
+			Grow ();
+		}
+	}
+	
+	void Grow ()
+	{
+		if (growthProgress == GROW_MAX) {
+			BeFullGrown ();
+		}
+		growthProgress += Time.deltaTime * growthSpeed;
+	}
+}
