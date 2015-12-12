661b073051583391018cc941804f94b9efbe467f
Yonas Berhe <yonas.berhe007@gmail.com>
2015-12-12 04:37:32 -0800

    stunned functin

--- Assets/Components/Player2/PlayerTwoRotation.cs
+++ Assets/Components/Player2/PlayerTwoRotation.cs
@@ -6,9 +6,35 @@ public class PlayerTwoRotation : MonoBehaviour {
 	[Range(1.0f, 9.0f)]
 	public float rotationSpeed = 9.0f;
 
+	public float timer = 3.0f;
+
+	public enum PlayerState{
+		PLAYING,
+		STUNNED,
+		DEAD
+	}
+
+	//reffering to our enum PlayState value Playing
+	//type of myState is PlayerState.playing
+	public PlayerState myState = PlayerState.PLAYING;
+
 	// Use this for initialization
 	void Start () {
 	
+	}
+	void OnTriggerEnter (Collider collider){
+		if (collider.gameObject.tag ==  "Seed") {
+			//here goes the co-routine
+			StartCoroutine(BeStunned());
+		}
+	}
+
+	IEnumerator BeStunned(){
+		myState = PlayerState.STUNNED;
+
+		yield return new WaitForSeconds (3.0f);
+
+		myState = PlayerState.PLAYING;
 	}
 	
 	// Update is called once per frame
@@ -17,5 +43,20 @@ public class PlayerTwoRotation : MonoBehaviour {
 			-Input.GetAxis ("Rotation.Vertical"),
 			Input.GetAxis ("Rotation.Horizontal"),
 			0));
+
+		/*switch (myState) {
+		case PlayerState.STUNNED:
+			timer -= Time.deltaTime;
+
+			if (timer < 0) {
+				//timer
+				myState = PlayerState.PLAYING;
+				timer = 3.0f;
+			}
+			break;
+		default:
+			break;
+
+		}*/
 	}
 }
