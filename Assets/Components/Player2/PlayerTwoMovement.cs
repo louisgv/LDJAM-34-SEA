a13228c95060b3f3624c7e35c3c0a1a1536fe57b
Yonas Berhe <yonas.berhe007@gmail.com>
2015-12-12 01:05:12 -0800

    in progress of making player 2 move

---
+++ Assets/Components/Player2/PlayerTwoMovement.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerTwoMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		transform.position = new Vector3(-2, 3, 0); 
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
