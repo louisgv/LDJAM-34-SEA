24e411bf7ed69716183301aad9b394ada51e8025
Louis Graham Vichy <lgvichy@gmail.com>
2015-12-12 15:30:23 -0800

    Publish Sound Controller
    Put sound method here. Prepare for Dragig flower, Fakes Sling Shot

---
+++ Assets/Components/Player2/PlayerTwoSounds.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerTwoSounds : MonoBehaviour
+{
+	public AudioSource mySource;
+
+	public AudioClip[] sounds;
+
+	void Start ()
+	{
+		mySource = GetComponentInParent<AudioSource> ();
+		
+	}
+	
+}
