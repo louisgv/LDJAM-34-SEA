24ccb61645247fa50215faf31fb016b1a5c7b11b
Louis Graham Vichy <lgvichy@gmail.com>
2015-12-12 23:03:52 -0800

    Preparing for Dragging Mechanic...

--- Assets/Components/GameManager/GameManager.cs
+++ Assets/Components/GameManager/GameManager.cs
@@ -30,13 +30,18 @@ public class GameManager : MonoBehaviour
 		// SHOW WINNER APPROPIRATELY	
 	}
 	
+	public static void Restart ()
+	{
+		Application.LoadLevel (Application.loadedLevel);
+	}
+	
 	public void Update ()
 	{
 		if (state == GameState.GAMEOVER) {
 			Time.timeScale = 0.0f;
 		}
 		if (Input.GetKeyDown (KeyCode.L)) {
-			Application.LoadLevel (Application.loadedLevel);
+			Restart ();
 		}
 	}
 	
