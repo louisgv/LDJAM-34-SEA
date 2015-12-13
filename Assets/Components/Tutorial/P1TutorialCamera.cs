4bac723a748def08248d35ae6dd7e00c90bd2ef7
Yonas Berhe <yonas.berhe007@gmail.com>
2015-12-13 05:17:59 -0800

    added login for p1 and p2 tags to look at each other

---
+++ Assets/Components/Tutorial/P1TutorialCamera.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class P1TutorialCamera : MonoBehaviour {
+
+	// Use this for initialization
+	public GameObject P2; 
+
+	void Start () {
+		transform.LookAt (P2.transform);
+	}
+	
+	// Update is called once per frame
+
+}
---
+++ Assets/Components/Tutorial/P2TutorialCamera.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class  P2TutorialCamera : MonoBehaviour {
+
+	// Use this for initialization
+	public GameObject P1; 
+
+	void Start () {
+		transform.LookAt (P1.transform);
+	}
+
+	// Update is called once per frame
+
+}
