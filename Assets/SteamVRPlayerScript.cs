40e7d9a1360de707f8a39999477aa5dbc7b79762
Louis Graham Vichy <lgvichy@gmail.com>
2015-12-14 12:19:44 -0800

    Nothing here....

--- Assets/SteamVRPlayerScript.cs
+++ Assets/SteamVRPlayerScript.cs
@@ -3,7 +3,8 @@ using System.Collections;
 using Valve.VR;
 using System.Collections.Generic;
 
-public class SteamVRPlayerScript : MonoBehaviour {
+public class SteamVRPlayerScript : MonoBehaviour
+{
 
 	public SteamVR_TrackedObject controllerLeftSlingshot;
 	public SteamVR_TrackedObject controllerRightLoader;
@@ -22,59 +23,59 @@ public class SteamVRPlayerScript : MonoBehaviour {
 		EVRButtonId.k_EButton_SteamVR_Trigger
 	};
 
-	List<int> controllerIndices = new List<int>();
+	List<int> controllerIndices = new List<int> ();
 	GameObject newSeed;
 
 	public GameObject seedPrefab;
 	public Vector3 seedPosition;
 
-	private void OnDeviceConnected(params object[] args)
+	private void OnDeviceConnected (params object[] args)
 	{
-		var index = (int)args[0];
+		var index = (int)args [0];
 
 		var vr = SteamVR.instance;
-		if (vr.hmd.GetTrackedDeviceClass((uint)index) != ETrackedDeviceClass.Controller)
+		if (vr.hmd.GetTrackedDeviceClass ((uint)index) != ETrackedDeviceClass.Controller)
 			return;
 
-		var connected = (bool)args[1];
-		if (connected)
-		{
-			Debug.Log(string.Format("Controller {0} connected.", index));
+		var connected = (bool)args [1];
+		if (connected) {
+			Debug.Log (string.Format ("Controller {0} connected.", index));
 			//PrintControllerStatus(index);
-			controllerIndices.Add(index);
-		}
-		else
-		{
-			Debug.Log(string.Format("Controller {0} disconnected.", index));
+			controllerIndices.Add (index);
+		} else {
+			Debug.Log (string.Format ("Controller {0} disconnected.", index));
 			//PrintControllerStatus(index);
-			controllerIndices.Remove(index);
+			controllerIndices.Remove (index);
 		}
 	}
 
-	void OnEnable()
+	void OnEnable ()
 	{
-		SteamVR_Utils.Event.Listen("device_connected", OnDeviceConnected);
+		SteamVR_Utils.Event.Listen ("device_connected", OnDeviceConnected);
 	}
 
-	void OnDisable()
+	void OnDisable ()
 	{
-		SteamVR_Utils.Event.Remove("device_connected", OnDeviceConnected);
+		SteamVR_Utils.Event.Remove ("device_connected", OnDeviceConnected);
 	}
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
 	
 	}
 
 	public Transform point, pointer;
 
 	[Range(2500f, 9000f)]
-	public float shootPower = 2500f; 
+	public float
+		shootPower = 2500f; 
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
 		//foreach (var index in controllerIndices) {
 		int index = (int)controllerLeftSlingshot.index;
-			/*foreach (int buttonId in axisIds) {
+		/*foreach (int buttonId in axisIds) {
 			if (SteamVR_Controller.Input (buttonId).GetTouchDown (buttonId))
 					Debug.Log (buttonId + " touch down");
 				if (SteamVR_Controller.Input (index).GetTouchUp (buttonId))
@@ -86,33 +87,36 @@ public class SteamVRPlayerScript : MonoBehaviour {
 			}*/
 
 		if (SteamVR_Controller.Input ((int)controllerRightLoader.index).GetPressDown (EVRButtonId.k_EButton_Axis0)) {
-				newSeed = (GameObject)GameObject.Instantiate(seedPrefab, controllerLeftSlingshot.transform.position, Quaternion.identity);
-				newSeed.GetComponent<Rigidbody> ().isKinematic = true;
-				Debug.Log ("B1" + " touch down" +  SteamVR_Controller.Input (0).transform.pos.ToString());
-			}
-		else if (SteamVR_Controller.Input ((int)controllerRightLoader.index).GetPress (EVRButtonId.k_EButton_Axis0) && newSeed != null) {
-				newSeed.transform.position = controllerRightLoader.transform.position;
-			}
-		else if (SteamVR_Controller.Input ((int)controllerRightLoader.index).GetPressUp (EVRButtonId.k_EButton_Axis0) && newSeed != null) {
-				//* if time alive less than 1 second detroy.
-				if (newSeed.GetComponent<SeedScript> ().LifeTimer < 0.5f) {
-					Destroy (newSeed);
-				} else {
-					newSeed.GetComponent<Rigidbody> ().isKinematic = false;
-				newSeed.GetComponent<Rigidbody> ().AddForce ((controllerLeftSlingshot.transform.position - controllerRightLoader.transform.position + nudgeVector) * shootPower);
-
-					newSeed = null;
-				}
-
-			}
-		else if (newSeed != null) {
+			newSeed = (GameObject)GameObject.Instantiate (seedPrefab, controllerLeftSlingshot.transform.position, Quaternion.identity);
+			newSeed.GetComponent<Rigidbody> ().isKinematic = true;
+			Debug.Log ("B1" + " touch down" + SteamVR_Controller.Input (0).transform.pos.ToString ());
+		} else if (SteamVR_Controller.Input ((int)controllerRightLoader.index).GetPress (EVRButtonId.k_EButton_Axis0) && newSeed != null) {
+			newSeed.transform.position = controllerRightLoader.transform.position;
+		} else if (SteamVR_Controller.Input ((int)controllerRightLoader.index).GetPressUp (EVRButtonId.k_EButton_Axis0) && newSeed != null) {
 			//* if time alive less than 1 second detroy.
 			if (newSeed.GetComponent<SeedScript> ().LifeTimer < 0.5f) {
 				Destroy (newSeed);
 			} else {
 				newSeed.GetComponent<Rigidbody> ().isKinematic = false;
-				newSeed.GetComponent<Rigidbody> ().AddForce ((controllerLeftSlingshot.transform.position - controllerRightLoader.transform.position + nudgeVector) * shootPower);
+				newSeed.GetComponent<Rigidbody> ().AddForce ((
+					controllerLeftSlingshot.transform.position - 
+					controllerRightLoader.transform.position + nudgeVector) 
+					* shootPower);
+				
+				newSeed = null;
+			}
 
+		} else if (newSeed != null) {
+			//* if time alive less than 1 second detroy.
+			if (newSeed.GetComponent<SeedScript> ().LifeTimer < 0.5f) {
+				Destroy (newSeed);
+			} else {
+				newSeed.GetComponent<Rigidbody> ().isKinematic = false;
+				newSeed.GetComponent<Rigidbody> ().AddForce ((
+					controllerLeftSlingshot.transform.position - 
+					controllerRightLoader.transform.position + nudgeVector) 
+					* shootPower
+				);
 				newSeed = null;
 			}
 
