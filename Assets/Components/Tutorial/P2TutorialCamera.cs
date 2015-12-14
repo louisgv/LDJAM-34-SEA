83cbf74a65c957d0edec4ef91fa9cdf6b11bd391
Yonas Berhe <yonas.berhe007@gmail.com>
2015-12-13 17:11:56 -0800

    message on player 2 closeness

--- Assets/Components/Tutorial/P2TutorialCamera.cs
+++ Assets/Components/Tutorial/P2TutorialCamera.cs
@@ -5,11 +5,25 @@ public class  P2TutorialCamera : MonoBehaviour {
 
 	// Use this for initialization
 	public GameObject P1; 
+	public GameObject [] CloseToTree; 
+
+	//playertwo
+	public PlayerTwoEvents playerTwoEvents;   
+
 
 	void Start () {
 		transform.LookAt (P1.transform);
+
+		playerTwoEvents = GetComponent<PlayerTwoEvents> ();
 	}
 
+	void Update () {
+
+		if (playerTwoEvents.state.Equals (PlayerTwoEvents.PlayerState.NEAR_FLOWER)) {
+			CloseToTree [0].SetActive (true);
+		}
+
+	}
 	// Update is called once per frame
 
 }
